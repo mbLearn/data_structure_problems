2425138b34475221e0d94dc7a7ed965fbc9413a4
mybNa <mybNa@users.noreply.github.com>
2020-07-28 20:58:11 -0700

    Update find_numbers_to_match_given_length.py

--- find_numbers_to_match_given_length.py
+++ find_numbers_to_match_given_length.py
@@ -27,3 +27,9 @@ def can_two_movies_fill_flight(movie_lengths, flight_length):
 # O(n) time, and 
 # O(n) space. 
 # Note while optimizing runtime we added a bit of space cost.
+
+# What We Learned
+# The trick was to use a set to access our movies by length, in O(1) time.
+
+# Using hash-based data structures, like dictionaries or sets, is so common in coding challenge solutions,
+# it should always be your first thought. Always ask yourself, right from the start: "Can I save time by using a dictionary?"
